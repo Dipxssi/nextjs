35aefefa8a4ab42e69b2122ebea873782fad338b
DIPSHA <dipshadas4@gmail.com>
2025-08-15 00:00:45 +0530

    Implementd prisma and the concept of singleton

--- next-app/app/api/v1/signup/route.ts
+++ next-app/app/api/v1/signup/route.ts
@@ -1,10 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import prismaClient from "../../../lib/db"
+
+const prismaClient = new PrismaClient();
+
 export async function POST(req : NextRequest) {
 //req.body?
   const data = await  req.json();
 
-  console.log(data);
+  prismaClient.user.create({
+    data:{
+      username :data.username,
+      password : data.password
+    }
+  })
   
   return NextResponse.json({
     message: "You have been signed up"
--- next-app/app/signup/page.tsx
+++ next-app/app/signup/page.tsx
@@ -1,22 +1,28 @@
 "use client" //bcuz of the presence of onClick handler
 import axios from "axios"
 import { useState } from "react"
+import { useRouter } from "next/navigation"
 export default function Signup() {
-  const [username,setUsername] = useState("");
-  const [password,setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
   return <div className="w-screen h-screen flex justify-center items-center">
     <div className="border p-2">
-      <input type="text" placeholder="username" onChange={ e => {
+      <input type="text" placeholder="username" onChange={e => {
         setUsername(e.target.value)
       }}></input>
-      <input type="password" placeholder="password" onChange={ e => {
+
+      <input type="password" placeholder="password" onChange={e => {
         setPassword(e.target.value)
       }}></input>
-      <button onClick={() => {
-        axios.post("http://localhost:3001/api/v1/signup",{
+
+      <button onClick={async () => {
+        await axios.post("http://localhost:3001/api/v1/signup", {
           username,
           password
         })
+        router.push("/signin")
       }}>Sign up</button>
     </div>
 
