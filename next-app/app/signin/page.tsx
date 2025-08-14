59adf807f099db5b71871d639a48a7eb266c9b4c
DIPSHA <dipshadas4@gmail.com>
2025-08-14 21:57:28 +0530

    Basic frontend and backend in next

---
+++ next-app/app/signin/page.tsx
@@ -0,0 +1,14 @@
+"use client" //bcuz of the presence of onClick handler
+import axios from "axios"
+export default function Signin() {
+  return <div className="w-screen h-screen flex justify-center items-center">
+    <div className="border p-2">
+      <input type="text" placeholder="username"></input>
+      <input type="password" placeholder="password"></input>
+      <button onClick={() => {
+        axios.post("http://localhost:3001/api/v1/signin")
+      }}>Sign in</button>
+    </div>
+
+  </div>
+}
